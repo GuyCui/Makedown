e3a4ce02e031dba165d9041ab2ba4af9996226a4
GuyCui <18024642@qq.com>
2021-03-22 22:06:59 +0800

    增加文件

---
+++ backup.sh
@@ -0,0 +1,5 @@
+# 添加和提交
+cd /Users/cuimingzhi/Library/Mobile\ Documents/com~apple~CloudDocs/Markdown && /Users/cuimingzhi/Library/Mobile\ Documents/com~apple~CloudDocs/Markdown git commit -a -m "daily backup"
+
+# 推送
+cd /Users/cuimingzhi/Library/Mobile\ Documents/com~apple~CloudDocs/Markdown && /Users/cuimingzhi/Library/Mobile\ Documents/com~apple~CloudDocs/Markdown git push origin master
